d719eb9cd75d883c561523a6b57ee98e579e0546
Doğukan ERSAN <96001974+DogukanErsan@users.noreply.github.com>
2022-10-21 17:46:28 +0300

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Rakamlar:" << endl;
+	int result = 0;
+
+
+	for (int i = 0;i < 10;i++) {
+		result += i;
+		cout << i << endl;
+
+	}
+	cout <<"Toplam Deger:" << result << endl;
+	return 0;
+}
