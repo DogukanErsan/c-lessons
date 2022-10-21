5d8b706b166d8290dba2a28a7a0ab0e6ae97295a
Doğukan ERSAN <96001974+DogukanErsan@users.noreply.github.com>
2022-10-21 17:57:23 +0300

    Add files via upload

---
+++ Example 3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Rakamlar:" << endl;
+	int result = 1;
+
+
+	for (int i = 1;i < 11;i++) {
+		result *= i;
+		cout << i << endl;
+
+	}
+	cout << "Toplam Deger:" << result << endl;
+	return 0;
+}
---
+++ Example 4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int userVal = 0;
+	string userValStr = "";
+	srand(time(NULL));
+	int rndNumber = rand() % 100;
+	cout << "Rastgele uretilen deger: " << rndNumber << "\n";
+
+	cout << "Please enter integer value: ";
+	cin >> userVal;
+
+	cout << "Please enter a text: ";
+	cin >> userValStr;
+	cout << "Entered integer Value by the user is: " << userVal << "\n";
+	cout << "Entered string Value by the user is: " << userValStr << "\n";
+
+}
+// cplusplus.com
+// stackoverflow.com
