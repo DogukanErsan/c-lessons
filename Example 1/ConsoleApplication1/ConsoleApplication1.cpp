3748508f53323c54846370edae9a66f2e2d7b02d
Doğukan ERSAN <96001974+DogukanErsan@users.noreply.github.com>
2022-10-21 17:53:49 +0300

    The number A, B equal to each other, greater or lesser example.

---
+++ Example 1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 5, b = 10;
+
+    if (a < b) {
+        cout << "A küçüktür B!\n";
+    }
+    else if (a > b) {
+        cout << "A Büyüktür B!\n";
+    }
+    else {
+        cout << "A Esittir B!\n";
+    }
+    return 0;
+
+
+
