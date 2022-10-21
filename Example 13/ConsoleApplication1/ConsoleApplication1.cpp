ba1fb1c153f8edf146eb228def1276b3e4437df7
Doğukan ERSAN <96001974+DogukanErsan@users.noreply.github.com>
2022-10-21 18:10:11 +0300

    Add files via upload

---
+++ Example 13/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <cmath>
+#include <locale.h>
+
+using namespace std;
+
+void show_progress_bar(int time, const std::string& message, char symbol)
+{
+    string progress_bar;
+    const double progress_level = 1.42;
+
+    cout << message << "\n\n";
+
+    for (double percentage = 0; percentage <= 100; percentage += progress_level)
+    {
+        progress_bar.insert(0, 1, symbol);
+        cout << "\r [" << ceil(percentage) << '%' << "] " << progress_bar;
+        this_thread::sleep_for(chrono::milliseconds(time));
+    }
+    cout << "\n\n";
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Turkish");
+    show_progress_bar(100, "Yükleniyor!", '-');
+}
